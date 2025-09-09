28b1a005d740ae1d484d70f16a8ff9863019c8c4
Guilherme <gui.ballestrim@gmail.com>
2025-09-09 11:13:10 -0300

    Classe Cliente e métodos (parte 1)
    Adicionei o metodo construtor, o metodo locar e o metodo devolver

--- classes.py
+++ classes.py
@@ -1,7 +1,7 @@
 class Item:
     #Construtor
     
-    def __init__(self, codigo=int, titulo=str):
+    def __init__(self, codigo: int, titulo: str):
         self.__codigo = codigo
         self.__titulo = titulo
         self.__disponivel = True
@@ -19,17 +19,40 @@ class Item:
         self.__disponivel = True
         print(f"Item {self.__disponivel} foi devolvido com sucesso.")
         
+# ------------------------------------------------------------------------------------------
+
 class Filme(Item):
     #Construtor
     
-    def __init__(self, codigo=int, titulo=str, genero=str, duracao=int):
+    def __init__(self, codigo: int, titulo: str, genero: str, duracao: int):
         Item.__init__(self, codigo, titulo)
         self.__genero = genero
         self.__duracao = duracao
-        
+
+# ------------------------------------------------------------------------------------------
+
 class Jogo(Item):
-    def __init__(self, codigo=int, titulo=str, plataforma=str, faixaEtaria=int):
+    #Construtor
+    
+    def __init__(self, codigo: int, titulo: str, plataforma: str, faixaEtaria: int):
         Item.__init__(self, codigo, titulo)
         self.__plataforma = plataforma
         self.__faixaEtaria = faixaEtaria
         
+# ------------------------------------------------------------------------------------------
+
+class Cliente:
+    #Construtor
+    
+    def __init__(self, nome: str, cpf: int, itensLocados=None):
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__itensLocados = itensLocados if itensLocados is not None else []
+        
+    #Métodos    
+    
+    def locar(item: Item):
+        Item.alugar()
+    
+    def devolver(item: Item):
+        Item.devolver()
