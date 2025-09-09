0cfeca97f84ea722b0092335732ec78810a712bc
Guilherme <gui.ballestrim@gmail.com>
2025-09-09 10:39:31 -0300

    Criação da classe Item
    Criação da classe Item, com seu método construtor e métodos Alugar e Devolver

--- classes.py
+++ classes.py
@@ -1,10 +1,10 @@
 class Item:
     #Construtor
     
-    def __init__ (self, codigo, titulo, disponivel):
+    def __init__(self, codigo=int, titulo=str):
         self.__codigo = codigo
         self.__titulo = titulo
-        self.__disponivel = disponivel
+        self.__disponivel = True
         
     #Métodos
     
@@ -18,3 +18,18 @@ class Item:
     def devolver(self):
         self.__disponivel = True
         print(f"Item {self.__disponivel} foi devolvido com sucesso.")
+        
+class Filme(Item):
+    #Construtor
+    
+    def __init__(self, codigo=int, titulo=str, genero=str, duracao=int):
+        Item.__init__(self, codigo, titulo)
+        self.__genero = genero
+        self.__duracao = duracao
+        
+class Jogo(Item):
+    def __init__(self, codigo=int, titulo=str, plataforma=str, faixaEtaria=int):
+        Item.__init__(self, codigo, titulo)
+        self.__plataforma = plataforma
+        self.__faixaEtaria = faixaEtaria
+        
