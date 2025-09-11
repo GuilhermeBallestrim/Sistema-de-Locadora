a546759614dc42981c7081c8243cc9aef305249e
Guilherme <gui.ballestrim@gmail.com>
2025-09-11 08:28:26 -0300

    Remoção de Try/Except
    Tirei alguns Try/Except redundantes do código

--- classes.py
+++ classes.py
@@ -5,13 +5,9 @@ class Item:
     # Construtor
 
     def __init__(self, codigo: int, titulo: str):
-        try:
-            self.__codigo = codigo
-            self.__titulo = titulo
-            self.__disponivel = True
-        except Exception as e:
-            print(f"Ocorreu um erro Inesperado: {e}")
-            os.system("pause")
+        self.__codigo = codigo
+        self.__titulo = titulo
+        self.__disponivel = True
 
     # Métodos
 
@@ -38,18 +34,10 @@ class Item:
             os.system("pause")
 
     def getTitulo(self):
-        try:
-            return self.__titulo
-        except Exception as e:
-            print(f"Ocorreu um erro Inesperado: {e}")
-            os.system("pause")
+        return self.__titulo
 
     def getCodigo(self):
-        try:
-            return self.__codigo
-        except Exception as e:
-            print(f"Ocorreu um erro Inesperado: {e}")
-            os.system("pause")
+        return self.__codigo    
 
 
 # ------------------------------------------------------------------------------------------
@@ -59,14 +47,9 @@ class Filme(Item):
     # Construtor
 
     def __init__(self, codigo: int, titulo: str, genero: str, duracao: int):
-        try:
-            Item.__init__(self, codigo, titulo)
-            self.__genero = genero
-            self.__duracao = duracao
-        except Exception as e:
-            print(f"Ocorreu um erro Inesperado: {e}")
-            os.system("pause")
-
+        Item.__init__(self, codigo, titulo)
+        self.__genero = genero
+        self.__duracao = duracao
 
 # ------------------------------------------------------------------------------------------
 
@@ -75,14 +58,9 @@ class Jogo(Item):
     # Construtor
 
     def __init__(self, codigo: int, titulo: str, plataforma: str, faixaEtaria: int):
-        try:
-            Item.__init__(self, codigo, titulo)
-            self.__plataforma = plataforma
-            self.__faixaEtaria = faixaEtaria
-        except Exception as e:
-            print(f"Ocorreu um erro Inesperado: {e}")
-            os.system("pause")
-
+        Item.__init__(self, codigo, titulo)
+        self.__plataforma = plataforma
+        self.__faixaEtaria = faixaEtaria
 
 # ------------------------------------------------------------------------------------------
 
@@ -91,49 +69,33 @@ class Cliente:
     # Construtor
 
     def __init__(self, nome: str, cpf: int, itensLocados=None):
-        try:
-            self.__nome = nome
-            self.__cpf = cpf
-            self.__itensLocados = itensLocados if itensLocados is not None else []
-        except Exception as e:
-            print(f"Ocorreu um erro Inesperado: {e}")
-            os.system("pause")
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__itensLocados = itensLocados if itensLocados is not None else []
 
     # Métodos
 
     def locar(self, item: Item):
-        try:
-            if item not in self.__itensLocados:
-                item.alugar()
-                self.__itensLocados.append(item)
-            else:
-                print("Este item já está locado por um cliente.")
-        except Exception as e:
-            print(f"Ocorreu um erro Inesperado: {e}")
-            os.system("pause")
+        if item not in self.__itensLocados:
+            item.alugar()
+            self.__itensLocados.append(item)
+        else:
+            print("Este item já está locado por um cliente.")
 
     def devolver(self, item: Item):
-        try:
-            if item in self.__itensLocados:
-                item.devolver()
-                self.__itensLocados.remove(item)
-            else:
-                print("Este item não está locado.")
-        except Exception as e:
-            print(f"Ocorreu um erro Inesperado: {e}")
-            os.system("pause")
+        if item in self.__itensLocados:
+            item.devolver()
+            self.__itensLocados.remove(item)
+        else:
+            print("Este item não está locado.")
 
     def listarItens(self):
-        try:
-            if self.__itensLocados:
-                print(f"Itens locados por {self.__nome}:")
-                for item in self.__itensLocados:
-                    print(f"- [{item.getCodigo()}] {item.getTitulo()}")
-            else:
-                print(f"{self.__nome} não possui itens locados.")
-        except Exception as e:
-            print(f"Ocorreu um erro Inesperado: {e}")
-            os.system("pause")
+        if self.__itensLocados:
+            print(f"Itens locados por {self.__nome}:")
+            for item in self.__itensLocados:
+                print(f"- [{item.getCodigo()}] {item.getTitulo()}")
+        else:
+            print(f"{self.__nome} não possui itens locados.")
     
     def getNome(self):
         return self.__nome
